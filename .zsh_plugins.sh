cf8fcb5231fe6fc0e9cd41dcf5fb6a65415ae83b
Pietro Di Bello <pierodibello@gmail.com>
2022-01-06 00:49:53 +0100

    add antibody zsh plugin list

---
+++ .zsh_plugins.sh
@@ -0,0 +1,20 @@
+ohmyzsh/ohmyzsh path:plugins/brew
+ohmyzsh/ohmyzsh path:plugins/git
+ohmyzsh/ohmyzsh path:plugins/git-extras
+ohmyzsh/ohmyzsh path:plugins/gitfast
+ohmyzsh/ohmyzsh path:plugins/history
+ohmyzsh/ohmyzsh path:plugins/mvn
+ohmyzsh/ohmyzsh path:plugins/autojump
+
+zsh-users/zsh-autosuggestions
+zsh-users/zsh-completions
+# zsh-users/zsh-syntax-highlighting
+# zsh-users/zsh-history-substring-search
+
+zdharma-continuum/fast-syntax-highlighting
+# nobeans/zsh-sdkman
+
+mafredri/zsh-async
+# sindresorhus/pure
+dfurnes/purer
+# danielefongo/shapeshift
